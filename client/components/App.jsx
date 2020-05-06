5e951e0f19ffbfe3cfb33a949b4613af250dcea0
Lucas santana <lucassantananz@hotmail.com>
2020-05-07 10:18:33 +1200

    added Route to profile, and added footer component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 
 import Landing from './Landing'
+import Profile from './Profile'
 
 const App = () => {
   return (
     <HashRouter>
       <Route exact path='/' component={Landing} />
+      <Route exact path='/profile' component={Profile}/>
     </HashRouter>
   )
 }
---
+++ client/components/Profile/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ProfileContainer extends React.Component {
+
+    }
+    render() {
+        return (
+            <div>
+               <h2>hello friend</h2>
+            </div>
+        )
+    }
+}
+
+export default ProfileContainer
