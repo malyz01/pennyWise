9d155f039a57ffe8ebe25f23abd21adfcd3c66ee
rkh-leung <leptanian@gmail.com>
2020-05-07 11:25:51 +1200

    Add DIY carousel

--- client/components/Landing/Carousel/Carousel.jsx
+++ client/components/Landing/Carousel/Carousel.jsx
@@ -1,19 +1,72 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
+import React, {Component} from "react";
+import './carousel.css'
 
-export default () => (
-  <Carousel autoPlay>
-    <div>
-      <img alt="Christy's fav girl" src="" />
-      <p className="legend">Legend 1</p>
+let items = [
+  'Tools To manage Your Expenses',
+  'Tools To manage Your Budget',
+  'Tools To manage Your Mom'
+]
+
+export default class Carousel extends Component {
+  state = {
+    currentIndex: 0,
+
+  }
+
+  prev = () => {
+    if (this.state.currentIndex > 0) {
+      this.setState({ currentIndex: this.state.currentIndex - 1 })
+    } else {
+      this.setState({currentIndex: items.length - 1})
+    }
+  }
+
+  next = () => {
+    if (this.state.currentIndex < items.length - 1) {
+      this.setState({ currentIndex: this.state.currentIndex + 1 })
+    } else {
+      this.setState({currentIndex: 0})
+    }
+  }
+
+  setIndex = index => {
+    this.setState({ currentIndex: index })
+  }
+
+  render() {
+    return (
+      <div className='carousel'>
+      <div className='carousel-main'>
+        <div
+          onClick={this.prev}
+          className={`arrows ${
+            this.state.currentIndex > 0 ? `visible` : `hidden`
+          }`}>
+          <i className='fas fa-arrow-circle-left'></i>
+        </div>
+        <p>{items[this.state.currentIndex]}</p>
+        <div
+          onClick={this.next}
+          className={`arrows ${
+            this.state.currentIndex < items.length - 1
+              ? `visible`
+              : `hidden`
+          }`}>
+          <i className='fas fa-arrow-circle-right'></i>
+        </div>
+      </div>
+      <div className='carousel-index'>
+        {items.map((item, index) => {
+          return (
+            <i key ={index}
+              onClick={() => this.setIndex(index)}
+              className={`fab fa-react ${
+                this.state.currentIndex === index ? `visible` : `hidden`
+              }`}></i>
+          )
+        })}
+      </div>
     </div>
-    <div>
-      <img alt="Christy's fav girl2" src="" />
-      <p className="legend">Legend 4</p>
-    </div>
-    <div>
-      <img alt="Christy's fav girl3" src="" />
-      <p className="legend">Legend 5</p>
-    </div>
-  </Carousel>
-)
+    )
+  }
+}
