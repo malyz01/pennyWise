0bf758e51540870bb3fc4b34aa2345e5ab8cc5b8
albasychris <albasychris@gmail.com>
2020-05-08 11:27:32 +1200

    add table in index.js

--- client/components/Expense/index.jsx
+++ client/components/Expense/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Header from './Header'
+import Table from './Table'
 
 export class index extends Component {
   render () {
     return (
       <div>
         <Header />
+        <Table />
       </div>
     )
   }
