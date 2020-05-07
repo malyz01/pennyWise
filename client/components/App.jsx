b26a8dbd2d69c3ee603cd1df5ddf5a2498f06315
albasychris <albasychris@gmail.com>
2020-05-07 12:32:18 +1200

    make a small comment in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 
 import Landing from '../components/Landing/Landing'
-
+// Hello there <3
 const App = () => {
   return (
     <HashRouter>
