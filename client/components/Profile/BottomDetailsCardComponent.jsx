ae8215429d8a478f6cd9cb8062acf9f3edb67230
Lucas santana <lucassantananz@hotmail.com>
2020-05-08 11:50:02 +1200

    added css property to borderComponent

--- client/components/Profile/BottomDetailsCardComponent.jsx
+++ client/components/Profile/BottomDetailsCardComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './profile.css'
 
 class BottomDetailsCardComponent extends React.Component {
   render () {
@@ -12,6 +13,36 @@ class BottomDetailsCardComponent extends React.Component {
                 manage your income
           </button>
 
+          <button className="ui right labeled icon button">
+            <i className="right arrow icon"></i>
+                manage your expenses
+          </button>
+
+          <button className="ui right labeled icon button">
+            <i className="right arrow icon"></i>
+                manage your budget
+          </button>
+
+          <button className="ui right labeled icon button">
+            <i className="right arrow icon"></i>
+                manage your goals
+          </button>
+        </div>
+
+        <div className='totalDisplayBox'>
+          <p className='totalDisplayTitle'>An Overview of your Finances</p>
+          <div className='totalOverview'>
+            <p>Total Income</p>
+            <p>Total Expenses</p>
+            <p>Total Budget</p>
+            <p>Total Goals</p>
+          </div>
+          <div className='moneyOverview'>
+            <p>$1,000,000</p>
+            <p>$999,999</p>
+            <p>$1 per week</p>
+            <p>10</p>
+          </div>
         </div>
 
       </div>
