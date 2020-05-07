1a5a03a0365ba264c3aa4ee8fb7729e86883b1bd
Lucas santana <lucassantananz@hotmail.com>
2020-05-08 11:21:23 +1200

    added buttons

--- client/components/Profile/BottomDetailsCardComponent.jsx
+++ client/components/Profile/BottomDetailsCardComponent.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 class BottomDetailsCardComponent extends React.Component {
   render () {
     return (
-      <button className="ui right labeled icon button">
-        <i className="right arrow icon"></i>
-        Next
-      </button>
+      <div className='bottomCardDetailsContainer'>
+
+        <div className='bottomButtonContainer'>
+
+          <button className="ui right labeled icon button">
+            <i className="right arrow icon"></i>
+                manage your income
+          </button>
+
+        </div>
+
+      </div>
     )
   }
 }
--- client/components/Profile/index.jsx
+++ client/components/Profile/index.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
-<<<<<<< HEAD
 import BottomDetailsCardComponent from './BottomDetailsCardComponent'
-=======
 import './profile.css'
->>>>>>> f953f96ea461ed4479086138dc42ed4754ffa57e
 
 class ProfileContainer extends React.Component {
   render () {
     return (
-<<<<<<< HEAD
-      <div>
-        <BottomDetailsCardComponent />
-=======
       <div className='profileMainContainer'>
-<<<<<<< HEAD
         <h1 className='profileHeading'>WELCOME TO YOUR PROFILE</h1>
         <div className='profileHeadingLine'></div>
         <p className='profileSummary'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-        <p className='profileSummary'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
->>>>>>> f953f96ea461ed4479086138dc42ed4754ffa57e
-=======
-        <div className='profileHeader'>
-          <h1 className='profileHeading'>WELCOME TO YOUR PROFILE</h1>
-          <div className='profileHeadingLine'></div>
-          <p className='profileSummary'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          <p className='profileSummary'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        <p className='profileSummary'>Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s.</p>
+
+        <div className='profileCardContainer'>
+          <BottomDetailsCardComponent />
         </div>
->>>>>>> 359d5fe2d3f4c806d1e3ce1a04adce44e19cb83b
       </div>
     )
   }
