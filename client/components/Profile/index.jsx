a90379b423f6dcf34bcd7cff0c469e96454bff6c
Lucas santana <lucassantananz@hotmail.com>
2020-05-08 10:33:08 +1200

    added few changes

--- client/components/Profile/index.jsx
+++ client/components/Profile/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
 class ProfileContainer extends React.Component {
-
-    }
-    render() {
-        return (
-            <div>
-               <h2>hello friend</h2>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <h2>hello friend</h2>
+      </div>
+    )
+  }
 }
 
 export default ProfileContainer
