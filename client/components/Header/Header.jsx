df587a6a67acae83296d9fb73786b2422d62aaaf
dd-tech-nz <ddumolo@hotmail.com>
2020-05-07 16:34:29 +1200

    complete header

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,31 +1,21 @@
-import React, { Component } from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/ToolBar'
-import MenuIcon from '@material-ui/icons/Menu'
+import React from 'react'
+import Carrousel from './Carrousel'
 import './header.css'
 
-class Header extends Component {
-  render () {
-    return (
-      <AppBar
-        position="fixed"
-        style={{
-          backgroundColor: '#2f2f2f',
-          boxShadow: 'none',
-          padding: '10px 0'
-        }}
-      >
-        <Toolbar>
-          <div className="header_logo">
-            <div className="font_righteous header header_logo_app">Pennywise</div>
-            <div className="header_logo_title">Budget - Your Game</div>
-          </div>
-          <MenuIcon />
-        </Toolbar>
+const Header = () => {
+  return (
+    <div className="carrousel" style={{ position: 'relative' }}>
+      <Carrousel />
 
-      </AppBar>
-    )
-  }
+      <div className="feature">
+        <div className="wrapper">
+
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default Header
