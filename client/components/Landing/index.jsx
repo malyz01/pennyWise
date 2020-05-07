9336f4a2f0f40d456e04dedeaa2797c599dcd3d1
albasychris <albasychris@gmail.com>
2020-05-07 16:49:37 +1200

    fix routing in app.jsx

--- client/components/Landing/index.jsx
+++ client/components/Landing/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Info from './Info'
 import Carousel from './Carousel'
+import Header from './Header'
 import './landing.css'
 
 const Landing = () => {
@@ -8,6 +9,7 @@ const Landing = () => {
     <div className = "landing">
 
       <div className = "landingBody">
+        <Header/>
         <Info />
         <Carousel/>
       </div>
