59897c9d76e3bcd8968bf10a03224fb7ede39c51
matthew-uy <32194180+malyz01@users.noreply.github.com>
2020-05-09 10:49:31 +1200

    Added selected and clear

--- client/store/reducers/expense.js
+++ client/store/reducers/expense.js
@@ -1,8 +1,10 @@
 import {
   GET_USER_EXPENSES,
   ADD_USER_EXPENSE,
+  SELECT_USER_EXPENSE,
   UPDATE_USER_EXPENSE,
-  DELETE_USER_EXPENSE
+  DELETE_USER_EXPENSE,
+  CLEAR_USER_EXPENSES
 } from '../types'
 
 const INITIAL_STATE = {
@@ -17,6 +19,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, all: payload }
     case ADD_USER_EXPENSE:
       return { ...state, all: [ ...state.all, payload ] }
+    case SELECT_USER_EXPENSE:
+      return { ...state, selected: payload }
     case UPDATE_USER_EXPENSE:
       const updated = state.all.map(e => { 
         if (e.id === payload.id) return payload
@@ -25,6 +29,7 @@ export default (state = INITIAL_STATE, action) => {
       return { all: updated, selected: payload }
     case DELETE_USER_EXPENSE:
       return { all: state.all.filter(e => e.id !== payload), selected: null }
+    case CLEAR_USER_EXPENSE:
     default:
       return state
   }
