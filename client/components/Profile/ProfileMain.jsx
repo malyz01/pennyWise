1454302772d827872161827a04902ef300667a30
Lucas santana <lucassantananz@hotmail.com>
2020-05-08 15:25:39 +1200

    fixed top layer of card :D

--- client/components/Profile/ProfileMain.jsx
+++ client/components/Profile/ProfileMain.jsx
@@ -4,12 +4,14 @@ const ProfileMain = (props) => {
   return (
     <div className='profileBody'>
       <div>
-        <div className='usernameAndEmail'>
-          <p className='profileFullName'>Matt Phillip Uy</p>
-          <p className='profileEmail'>MattUy@Hotmail.com</p>
+        <div className='fullNameAndEmail'>
+          <div className='profileFullName'>Matt Phillip Uy</div>
+          <div className='profileEmail'>MattUy@Hotmail.com</div>
         </div>
-        <button className="ui primary button">edit profile</button>
-        <button className="ui primary button">subscribe email</button>
+        <div className='topButtons'>
+          <button className="ui primary button edit">edit profile</button>
+          <button className="ui primary button sub">subscribe email</button>
+        </div>
       </div>
       <img className="profileAvatar" src="/images/profileAvatar.jpg" alt="this do be the profile avatar"/>
     </div>
