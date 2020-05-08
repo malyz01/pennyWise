cf3aaeb20e94d688478980ecbb484cbf02929cd1
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-08 12:40:41 +1200

    Add post route for getting user details

--- server/server.js
+++ server/server.js
@@ -10,5 +10,6 @@ server.use(express.json())
 server.use(express.static(path.resolve('server', 'public')))
 
 server.use('/api/v1/auth', require('./routes/auth'))
+server.use('/api/v1/users', require('./routes/profiles'))
 
 module.exports = server
