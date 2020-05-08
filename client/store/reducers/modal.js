605c2e3ed7a8eecf8bc438c996044c45b65ba6ee
malyz <mattuy001@gmail.com>
2020-05-09 07:52:07 +1200

    added in income and expense to modal actions creator and reducer

--- client/store/reducers/modal.js
+++ client/store/reducers/modal.js
@@ -1,10 +1,22 @@
 import {
   SET_MODAL_AUTH_OPEN,
-  SET_MODAL_AUTH_FORM
+  SET_MODAL_AUTH_FORM,
+  SET_MODAL_EXPENSE_FORM,
+  SET_MODAL_EXPENSE_OPEN,
+  SET_MODAL_INCOME_FORM,
+  SET_MODAL_INCOME_OPEN
 } from '../types'
 
 const INITIAL_STATE = {
   auth: {
+    form: '',
+    open: false
+  },
+  expense: {
+    form: '',
+    open: false
+  },
+  income: {
     form: '',
     open: false
   }
@@ -13,9 +25,29 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_MODAL_AUTH_OPEN:
-      return { ...state, auth: { ...state.auth, open: action.payload } }
+      return { ...INITIAL_STATE, auth: { ...state.auth, open: action.payload } }
     case SET_MODAL_AUTH_FORM:
-      return { ...state, auth: { ...state.auth, form: action.payload } }
+      return { ...INITIAL_STATE, auth: { ...state.auth, form: action.payload } }
+    case SET_MODAL_EXPENSE_OPEN:
+      return {
+        ...INITIAL_STATE,
+        expense: { ...state.expense, open: action.payload }
+      }
+    case SET_MODAL_EXPENSE_FORM:
+      return {
+        ...INITIAL_STATE,
+        expense: { ...state.expense, form: action.payload }
+      }
+    case SET_MODAL_INCOME_OPEN:
+      return {
+        ...INITIAL_STATE,
+        income: { ...state.income, open: action.payload }
+      }
+    case SET_MODAL_INCOME_FORM:
+      return {
+        ...INITIAL_STATE,
+        income: { ...state.income, form: action.payload }
+      }
     default:
       return state
   }
