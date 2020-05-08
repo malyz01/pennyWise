e570dfb784cab86c3ab039c1d6e43a068f1a5015
matthew-uy <32194180+malyz01@users.noreply.github.com>
2020-05-09 10:36:23 +1200

    Changed to object for initial state and modified switched statements

--- client/store/reducers/expense.js
+++ client/store/reducers/expense.js
@@ -1,21 +1,26 @@
 import {
-  GET_USER_EXPENSE,
+  GET_USER_EXPENSES,
   ADD_USER_EXPENSE,
   UPDATE_USER_EXPENSE,
   DELETE_USER_EXPENSE
 } from '../types'
 
-const INITIAL_STATE = []
+const INITIAL_STATE = {
+  all: [],
+  selected: null
+}
 
 export default (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case GET_USER_EXPENSE:
-      return action.payload
+  const { type, payload } = action
+  switch (type) {
+    case GET_USER_EXPENSES:
+      return { ...state, all: payload }
     case ADD_USER_EXPENSE:
+      return { ...state, all: [ ...state.all, payload ] }
     case UPDATE_USER_EXPENSE:
-      return [...state, action.payload]
+      return { all: [ ...state.all, payload ], selected: payload }
     case DELETE_USER_EXPENSE:
-      return state.filter((expense) => expense.id !== Number(action.payload))
+      return { all: state.all.filter(e => e.id !== payload), selected: null }
     default:
       return state
   }
