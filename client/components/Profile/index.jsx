5c3e1b9be8e363037fd79d8a61235849fb5e534b
Lucas santana <lucassantananz@hotmail.com>
2020-05-08 12:13:07 +1200

    testing

--- client/components/Profile/index.jsx
+++ client/components/Profile/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import BottomDetailsCardComponent from './BottomDetailsCardComponent'
 import './profile.css'
-// import './index.css'
+
 import ProfileMain from './ProfileMain'
+import Footer from '../Footer/index.jsx'
 
 class ProfileContainer extends React.Component {
   render () {
@@ -14,12 +15,14 @@ class ProfileContainer extends React.Component {
           <p className='profileSummary'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
           <p className='profileSummary'>Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s.</p>
         </div>
-        
+
         <div className='profileCardContainer'>
           <ProfileMain />
           <BottomDetailsCardComponent />
+
         </div>
       </div>
+
     )
   }
 }
