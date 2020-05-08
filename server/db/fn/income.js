ef161e344a4b4dd879ec31721135052634f2c718
albasychris <albasychris@gmail.com>
2020-05-09 08:52:43 +1200

    add a getIncomes route

--- server/db/fn/income.js
+++ server/db/fn/income.js
@@ -1,5 +1,10 @@
 const conn = require('../connection')
 
+function getIncomes (db = onn) {
+  return db('income')
+    .select()
+}
+
 function getIncome (userId, db = conn) {
   return db('income')
     .where('user_id', userId)
@@ -8,13 +13,26 @@ function getIncome (userId, db = conn) {
 function addIncome (data, db = conn) {
   return db('income')
     .insert(data)
+    .then(([id]) => db('income').where('id', id).select().first())
+    .catch(err => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
 }
 
 // Not too certain
 function updateIncome (data, db = conn) {
+  // eslint-disable-next-line no-console
+  console.log(data)
+
   return db('income')
     .where('user_id', data.userId)
     .update(data)
+    .then(() => db('income').where('id', data.userId).select().first())
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    })
 }
 
 function deleteIncome (userId, db = conn) {
@@ -25,6 +43,7 @@ function deleteIncome (userId, db = conn) {
 
 module.exports = {
   getIncome,
+  getIncomes,
   addIncome,
   updateIncome,
   deleteIncome
--- server/routes/income.js
+++ server/routes/income.js
@@ -3,11 +3,35 @@ const camelcaseKeys = require('camelcase-keys')
 
 const db = require('../db/fn/income')
 
+// GET - /api/v1/income/
 router.get('/', (req, res) => {
   return db
-    .getIncome()
+    .getIncomes()
     .then(camelcaseKeys)
     .then((income) => res.json(income))
+    .catch(() => res.sendStatus(500))
 })
+// GET - /api/v1/income/:userId
+// router.get('/:userId', (req, res) => {
+//   return db
+//     .getIncome(req.params.userId)
+//     .then(camelcaseKeys)
+//     .then((income) => res.json(income))
+//     .catch(() => res.sendStatus(500))
+// })
+// // POST /api/v1/income/:userId
+// router.post('/:userId', (req, res) => {
+//   return db
+//     .addIncome(req.params.userId, req.body)
+//     .then(income => res.sendStatus(200).json(income))
+//     .catch(() => res.sendStatus(500))
+// })
 
+// // PUT /api/v1/income/:userId
+// router.put('/:incomeId', (req, res) => {
+//   return db
+//     .updateIncome(req.params.incomeId, req.body)
+//     .then(income => res.sendStatus(200).json(income))
+//     .catch(() => res.sendStatus(500))
+// })
 module.exports = router
