3e16bbc45220dcf2537dc2bd5076e0869a6495f3
malyz <mattuy001@gmail.com>
2020-05-10 08:38:39 +1200

    fixed to use setModalOpen and setModalName

--- client/store/actions/modal.js
+++ client/store/actions/modal.js
@@ -1,15 +1,4 @@
-import {
-  SET_MODAL_OPEN,
-  SET_MODAL_NAME,
-  SET_MODAL_AUTH_OPEN,
-  SET_MODAL_AUTH_FORM,
-  SET_MODAL_EXPENSE_OPEN,
-  SET_MODAL_EXPENSE_FORM,
-  SET_MODAL_INCOME_OPEN,
-  SET_MODAL_INCOME_FORM,
-  SET_MODAL_GOAL_OPEN,
-  SET_MODAL_GOAL_FORM
-} from '../types'
+import { SET_MODAL_OPEN, SET_MODAL_NAME } from '../types'
 
 export const setModalOpen = (open) => ({
   type: SET_MODAL_OPEN,
@@ -20,48 +9,3 @@ export const setModalName = (name) => ({
   type: SET_MODAL_NAME,
   payload: name
 })
-
-export const setModalAuthOpen = (bool) => ({
-  type: SET_MODAL_AUTH_OPEN,
-  payload: bool
-})
-
-export const setModalAuthForm = (form) => ({
-  type: SET_MODAL_AUTH_FORM,
-  payload: form
-})
-
-export const setModalExpenseOpen = (bool) => ({
-  type: SET_MODAL_EXPENSE_OPEN,
-  payload: bool
-})
-
-export const setModalExpenseForm = (form) => ({
-  type: SET_MODAL_EXPENSE_FORM,
-  payload: form
-})
-
-export const setModalIncomeOpen = (bool) => ({
-  type: SET_MODAL_INCOME_OPEN,
-  payload: bool
-})
-
-export const setModalIncomeForm = (form) => ({
-  type: SET_MODAL_INCOME_FORM,
-  payload: form
-})
-
-export const setModalGoalOpen = (bool) => ({
-  type: SET_MODAL_GOAL_OPEN,
-  payload: bool
-})
-
-export const setModalGoalForm = (form) => ({
-  type: SET_MODAL_GOAL_FORM,
-  payload: form
-})
-
-export default {
-  setModalAuthOpen,
-  setModalAuthForm
-}
--- client/store/reducers/modal.js
+++ client/store/reducers/modal.js
@@ -1,35 +1,8 @@
-import {
-  SET_MODAL_OPEN,
-  SET_MODAL_NAME,
-  SET_MODAL_AUTH_OPEN,
-  SET_MODAL_AUTH_FORM,
-  SET_MODAL_EXPENSE_FORM,
-  SET_MODAL_EXPENSE_OPEN,
-  SET_MODAL_INCOME_FORM,
-  SET_MODAL_INCOME_OPEN,
-  SET_MODAL_GOAL_FORM,
-  SET_MODAL_GOAL_OPEN
-} from '../types'
+import { SET_MODAL_OPEN, SET_MODAL_NAME } from '../types'
 
 const INITIAL_STATE = {
   name: null,
-  open: false,
-  auth: {
-    form: '',
-    open: false
-  },
-  expense: {
-    form: '',
-    open: false
-  },
-  income: {
-    form: '',
-    open: false
-  },
-  goal: {
-    form: '',
-    open: false
-  }
+  open: false
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -38,40 +11,6 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, open: action.payload }
     case SET_MODAL_NAME:
       return { ...state, name: action.payload }
-    case SET_MODAL_AUTH_OPEN:
-      return { ...INITIAL_STATE, auth: { ...state.auth, open: action.payload } }
-    case SET_MODAL_AUTH_FORM:
-      return { ...INITIAL_STATE, auth: { ...state.auth, form: action.payload } }
-    case SET_MODAL_EXPENSE_OPEN:
-      return {
-        ...INITIAL_STATE,
-        expense: { ...state.expense, open: action.payload }
-      }
-    case SET_MODAL_EXPENSE_FORM:
-      return {
-        ...INITIAL_STATE,
-        expense: { ...state.expense, form: action.payload }
-      }
-    case SET_MODAL_INCOME_OPEN:
-      return {
-        ...INITIAL_STATE,
-        income: { ...state.income, open: action.payload }
-      }
-    case SET_MODAL_INCOME_FORM:
-      return {
-        ...INITIAL_STATE,
-        income: { ...state.income, form: action.payload }
-      }
-    case SET_MODAL_GOAL_OPEN:
-      return {
-        ...INITIAL_STATE,
-        goal: { ...state.goal, open: action.payload }
-      }
-    case SET_MODAL_GOAL_FORM:
-      return {
-        ...INITIAL_STATE,
-        goal: { ...state.goal, form: action.payload }
-      }
     default:
       return state
   }
