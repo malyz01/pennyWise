46d1e52805d62f6092401c805604bf7556af01b1
Lucas santana <lucassantananz@hotmail.com>
2020-05-09 17:26:47 +1200

    added header for index.js budget

--- client/components/Budget/index.jsx
+++ client/components/Budget/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './budget.css'
 
 class Budget extends React.Component {
   render () {
     return (
-      <div>Hello!</div>
+      <div className='budgetContainer'>
+        <div className='budgetHeaderContainer'>
+          <h1 className="budgetHeader">MANAGE YOUR BUDGETS AND SAVINGS HERE</h1>
+          <div className="BudgetHeaderLine"></div>
+          <div className="budgetSubHeader">
+
+            <p className="budgetPTags">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <p className="budgetPTags">Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s.</p>
+
+          </div>
+        </div>
+      </div>
     )
   }
 }
