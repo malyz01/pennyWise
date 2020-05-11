2cad2f6a4136ff2a203da3c2c286baf50ac64683
albasychris <albasychris@gmail.com>
2020-05-11 16:13:59 +1200

    complete display of goals data on bar graph

--- client/components/Budget/BudgetCard.jsx
+++ client/components/Budget/BudgetCard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import './budget.css'
-
+import { connect } from 'react-redux'
 import h from './helper'
 
 const options = [
@@ -9,7 +9,7 @@ const options = [
   { key: 'm', value: 'Monthly', text: 'Monthly' },
   { key: 'a', value: 'Annually', text: 'Annually' }
 ]
-
+let count = 0
 class BudgetCard extends Component {
   state = {
     frequency: 'Weekly'
@@ -57,20 +57,40 @@ class BudgetCard extends Component {
   onChange = (e, { name, value }) => {
     this.setState({ [name]: value })
   }
-  getColor = () => {
-    const getRandom = () => {
-      return Math.floor(Math.random() * 255)
+  getColor = (number) => {
+    const getShade = (minR, minB, minG, maxR, maxB, maxG) => {
+      return `rgb(${Math.floor(Math.random() * (maxR - minR) + minR)},${Math.floor((Math.random() * (maxB - minB) + minB))},${Math.floor((Math.random() * (maxG - minG) + minG))})`
     }
-    return `rgb(${getRandom()},${getRandom()},${getRandom()})`
+    const getWhite = (min, max) => {
+      const shade = Math.floor(Math.random() * (max - min) + min)
+      return `rgb(${shade},${shade},${shade})`
+    }
+    switch (number) {
+      case 0:
+        count++
+        return '#9d00e6'
+      case 1:
+        count++
+        return '#c64dff'
+      case 2:
+        count++
+        return '#cd7eb8'
+      case 3:
+        count = 0
+        return '#9326ff'
+    }
   }
   render () {
+    console.log('goals', this.props.goal)
     return (
-      <div className = "budgetCardMain">
+      <div className="budgetCardMain">
         <div className="topCardComponent">
           <div className="topCardWrapper">
             <div className="budgetRow">
               <div className="colLeft">
-                <h2 style={{ color: 'white' }}>Frequency</h2>
+                < h2 style={{ color: 'white' }}>
+                  Frequency
+                </h2>
               </div>
               <div className="colRight">
                 <Dropdown
@@ -91,7 +111,7 @@ class BudgetCard extends Component {
               </div>
               <div className="colRight">
                 <h2 style={{ color: 'white' }}>
-                $ {this.renderTotalBudget()}
+                  $ {this.renderTotalBudget()}
                 </h2>
               </div>
             </div>
@@ -103,7 +123,7 @@ class BudgetCard extends Component {
               </div>
               <div className="colRight">
                 <h2 style={{ color: 'white' }}>
-                $ {this.renderTotalContribution()}
+                  $ {this.renderTotalContribution()}
                 </h2>
               </div>
             </div>
@@ -115,29 +135,33 @@ class BudgetCard extends Component {
               </div>
               <div className="colRight">
                 <h2 style={{ color: 'white' }}>
-                $ {this.renderUnallocatedBudget()}
+                  $ {this.renderUnallocatedBudget()}
                 </h2>
               </div>
             </div>
           </div>
         </div>
-        <div className = "budgetGraphs">
+        <div className="budgetGraphs">
           {
-            [1, 2, 3, 4, 5].map((item, index) => {
+            this.props.goal.map((item, index) => {
               return (
                 <>
-              <div key={item} className = "budgetGraphRow">
-                <div className = "budgetGraphLabel">
-                  <p>Lucas new runescape account</p>
-                </div>
+                  <div key={item.id} className="budgetGraphRow">
+                    <div className="budgetGraphLabel">
+                      <p>{item.goalName}</p>
+                    </div>
 
-                <div className = "budgetGraphBar" style={{ background: this.getColor }}>
+                    <div className="budgetGraphBar" style={{
+                      background: this.getColor(count),
+                      // width: '50%'
+                      width: `${item.currentAmount / item.targetBudget * 100}%`
+                    }}>
 
-                </div>
+                    </div>
 
-              </div>
-              {index < 4 && <hr/>}
-              </>)
+                  </div>
+                  {index < this.props.goal.length - 1 && <hr />}
+                </>)
             })
           }
 
