69173308d9920b1dfd7b9f4d4b8f8f788f63ff48
malyz <mattuy001@gmail.com>
2020-05-11 21:05:05 +1200

    clean up some unused action creators

--- client/store/reducers/user.js
+++ client/store/reducers/user.js
@@ -1,16 +1,13 @@
-import { GET_USER_PROFILE, GET_USER_DETAILS } from '../types'
+import { GET_USER_PROFILE } from '../types'
 
 const INITIAL = {
-  profile: null,
-  userData: null
+  profile: null
 }
 
 export default (state = INITIAL, action) => {
   switch (action.type) {
     case GET_USER_PROFILE:
       return { ...state, profile: action.payload }
-    case GET_USER_DETAILS:
-      return { ...state, userData: action.payload }
     default:
       return state
   }
