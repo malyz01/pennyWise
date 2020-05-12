b2a6eb0b1adf4fb59082fe9d859c3ce443fecb7b
albasychris <albasychris@gmail.com>
2020-05-12 15:16:00 +1200

    fix budget features table

--- client/components/Budget/Goals/GoalsTable.jsx
+++ client/components/Budget/Goals/GoalsTable.jsx
@@ -10,12 +10,21 @@ class GoalsTable extends Component {
     this.props.selectUserGoal(goal)
   }
 
-  getRemaining = (end) => {
+  getRemaining = (end, type) => {
     const date1 = new Date(end)
     const date2 = Date.now()
     const diffTime = date1 - date2
 
-    return Math.floor(diffTime / (1000 * 60 * 60 * 24))
+    const remaining = Math.floor(diffTime / (1000 * 60 * 60 * 24))
+    if (type === 'string') {
+      if (remaining > 1) {
+        return `${remaining} days`
+      } else if (remaining <= 0) {
+        return `completed!`
+      }
+      return remaining
+    }
+    return remaining
   }
 
   getDailyContribution = (amount, frequency) => {
@@ -50,12 +59,18 @@ class GoalsTable extends Component {
     const completionDate = new Date(Number(currentDate) + Number(millisecondsLeft))
 
     const formatedDate = completionDate.toLocaleDateString(undefined, { year: 'numeric', month: 'numeric', day: 'numeric' })
-    const segments = formatedDate.split('/')
-    return `${segments[1]}/${segments[0]}/${segments[2]}`
+
+    return formatedDate
   }
-  formatDate = (date) => {
-    const segments = date.split('-')
-    return `${segments[2]}/${segments[1]}/${segments[0]}`
+  formatDate = (date, type) => {
+    if (type === 'chosen') {
+      const segments = date.split('-')
+      return `${segments[2]}/${segments[1]}/${segments[0]}`
+    }
+    if (type === 'actual') {
+      const segments = date.split('/')
+      return `${segments[1]}/${segments[0]}/${segments[2]}`
+    }
   }
   render () {
     return (
@@ -81,10 +96,10 @@ class GoalsTable extends Component {
                   <Table.Cell>{goal.goalName}</Table.Cell>
                   <Table.Cell>${goal.targetBudget}</Table.Cell>
                   <Table.Cell>${goal.currentAmount}</Table.Cell>
-                  <Table.Cell>{`${this.getActualDate(goal.budgetDistribution, goal.frequency, goal.targetBudget, goal.currentAmount)}`}</Table.Cell>
-                  <Table.Cell>{`${this.getRemaining(this.getActualDate(goal.budgetDistribution, goal.frequency, goal.targetBudget, goal.currentAmount))} ${this.getRemaining(this.getActualDate(goal.budgetDistribution, goal.frequency, goal.targetBudget, goal.currentAmount)) > 1 ? 'Days' : 'Day'} `}</Table.Cell>
+                  <Table.Cell>{`${this.formatDate(this.getActualDate(goal.budgetDistribution, goal.frequency, goal.targetBudget, goal.currentAmount), 'actual')}`}</Table.Cell>
+                  <Table.Cell>{this.getRemaining(this.getActualDate(goal.budgetDistribution, goal.frequency, goal.targetBudget, goal.currentAmount), 'string')}</Table.Cell>
                   <Table.Cell>${this.getWeeklyContribution(goal.budgetDistribution, goal.frequency).toFixed(2)}</Table.Cell>
-                  <Table.Cell className="goalsTableSpecial">{this.formatDate(goal.targetDate)}</Table.Cell>
+                  <Table.Cell className="goalsTableSpecial">{this.formatDate(goal.targetDate, 'chosen')}</Table.Cell>
                   <Table.Cell className="goalsTableSpecial">{getWeeklyContribution(this.getRemaining(goal.targetDate), goal.currentAmount, goal.targetBudget)}</Table.Cell>
                   <Table.Cell>{goal.active ? 'Yes' : 'No'}</Table.Cell>
                 </Table.Row>
