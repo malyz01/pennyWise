97b6120b09380ff48ed313578c6b317f2b485b99
albasychris <albasychris@gmail.com>
2020-05-13 09:26:08 +1200

    clean up code

--- client/components/Landing/index.jsx
+++ client/components/Landing/index.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
-import Info from './Info'
-import Carousel from './Carousel'
 import Header from './Header'
 import './landing.css'
-import Slide from 'react-reveal/Slide'
 import Content from './Content'
 const Landing = () => {
   return (
     <div className = "landing">
       <Header/>
       <Content />
-      {/* <div className='landingSubContainer'>
-        <div>
-          <Slide duration={500} left>
-            <Info />
-          </Slide>
-        </div>
-        <div>
-          <Slide duration={500} delay={500} right>
-            <Carousel />
-          </Slide>
-        </div>
-
-      </div> */}
     </div>
   )
 }
