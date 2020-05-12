fa301ae04e59b6082abcf0105aa25a2032e75e10
albasychris <albasychris@gmail.com>
2020-05-12 21:20:33 +1200

    complete navbar toggling

--- client/components/Navbar/index.jsx
+++ client/components/Navbar/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './navbar.css'
-
+import { Icon } from 'semantic-ui-react'
 import LogoMenu from './LogoMenu'
 import MenuBtn from './MenuBtn'
 
 const Navbar = () => {
+  const [navbarToggle, setNavbarToggle] = useState(true)
+
+  const handleClick = () => {
+    setNavbarToggle(!navbarToggle)
+  }
+
   return (
-    <div className='navBarMainContainer'>
-      <LogoMenu />
-      <MenuBtn />
-    </div>
+    <>
+      {!navbarToggle && <div className="navToggleOn" onClick={handleClick}><Icon name="chevron circle down"/></div>}
+      <div className={`navBarMainContainer ${navbarToggle ? 'navOn' : 'navOff'}`}>
+        <div onClick={handleClick}className='navToggle'><Icon name ="arrow up"/></div>
+        <LogoMenu />
+        <MenuBtn />
+      </div>
+    </>
   )
 }
 
