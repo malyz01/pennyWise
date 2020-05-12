345717176c4223bfd21b63b8ab11a755144a0251
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-12 15:02:55 +1200

    create tebleHelper tests

---
+++ client/components/Budget/Goals/tableHelper.test.js
@@ -0,0 +1,25 @@
+import { getWeeklyContribution, getMonthlyContribution, getAnnualContribution } from './tableHelper'
+
+test('getWeeklyContribution returns the correct result', () => {
+  const daysNumber = 35
+  const currentAmount = 200
+  const budget = 12000
+  const result = getWeeklyContribution(daysNumber, currentAmount, budget)
+  expect(result).toBe(2360)
+})
+
+test('getMonthlyContribution returns the correct result', () => {
+  const daysNumber = 365
+  const currentAmount = 200
+  const budget = 12000
+  const result = getMonthlyContribution(daysNumber, currentAmount, budget)
+  expect(result).toBe(983.33)
+})
+
+test('getAnnualContribution returns the correct result', () => {
+  const daysNumber = 2920
+  const currentAmount = 200
+  const budget = 12000
+  const result = getAnnualContribution(daysNumber, currentAmount, budget)
+  expect(result).toBe(1475)
+})
