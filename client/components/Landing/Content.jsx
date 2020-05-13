470733a3a9018be695e553f9f3227e00944fb867
dd-tech-nz <ddumolo@hotmail.com>
2020-05-13 13:54:29 +1200

    add all animations

--- client/components/Landing/Content.jsx
+++ client/components/Landing/Content.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Container } from 'semantic-ui-react'
 import './content.css'
+import Slide from 'react-reveal/Slide'
 
 import Carousel from './Carousel'
 import { setModalOpen, setModalName } from '../../store/actions/modal'
@@ -15,33 +16,37 @@ const Content = (props) => {
   return (
     <div className="landingContent">
       <Container>
-        <div className="landingContentJoin">
-          <div className="landingInfoHeading">
-            <h1>Join the Wise</h1>
+        <Slide left duration={500}>
+          <div className="landingContentJoin">
+            <div className="landingInfoHeading">
+              <h1>Join the Wise</h1>
+            </div>
+            <hr className="landingInfoHr" />
+            <div className="landingInfoList">
+              <div className="landginInfoBody">
+                - COVID-19 information regarding you and your money
+              </div>
+              <div className="landginInfoBody">
+                - Your money, mindfulness and this moment
+              </div>
+              <div className="landginInfoBody">
+                - Too many of us worry about our money situations. Sorting your
+                money is good for your mental health too.
+              </div>
+              {!props.isAuthenticated && (
+                <button
+                  onClick={handleOnClick}
+                  className="ui button landingSignUp"
+                >
+                  Sign Up
+                </button>
+              )}
+            </div>
           </div>
-          <hr className="landingInfoHr" />
-          <div className="landingInfoList">
-            <div className="landginInfoBody">
-              - COVID-19 information regarding you and your money
-            </div>
-            <div className="landginInfoBody">
-              - Your money, mindfulness and this moment
-            </div>
-            <div className="landginInfoBody">
-              - Too many of us worry about our money situations. Sorting your
-              money is good for your mental health too.
-            </div>
-            {!props.isAuthenticated && (
-              <button
-                onClick={handleOnClick}
-                className="ui button landingSignUp"
-              >
-                Sign Up
-              </button>
-            )}
-          </div>
-        </div>
-        <Carousel />
+        </Slide>
+        <Slide right duration={500} delay={500}>
+          <Carousel />
+        </Slide>
       </Container>
     </div>
   )
